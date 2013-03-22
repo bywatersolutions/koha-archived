f0a1239911fff6a67d032c1a6acd8c386615f2b7
Brendan <info@bywatersolutions.com>
2013-03-22 02:22:11 -0700

    BWS CUSTOM - Bug 7736 - Edifact QUOTE and ORDER functionality
    This is a patch brining together the original
    work by Mark Gavillet and the subsequent patches
    by Mark, Martin Renvoize and myself
    
    It contains the code as currently runnng in
    production
    
    In addition I've added updatedatebase entries to
    installer and replaced the hardcoded currency with
    a currency extraction. As the db currently does not
    force the standard codes we check if that is 3 uppercase
    ascii characters before using it.
    
    Elliott Davis:
    Added unit test for EDI
    
    Zeno Tajoli:
    Rebased on last master git tree.
    I have change only the file installer/data/mysql/sysprefs.sql
    Added missing end tag for tools-home.tt
    
    Brendan Gallagher:
    Rebased on current master
    Changed installer/data/mysql/sysprefs.sql
    koha-tmpl/intranet-tmpl/prog/en/modules/acqui/basket.tt
    
    Bug 7736 - Edifact QUOTE and ORDER functionality - Followup - cordinate this with template tools-home.tt
    
    After the writting of the patch, the template tools-home.tt has a little
    change.
    
    I have have tested the ench wit instruction and I confirm that works.
    Attention on dir misc/edi_files, you need to give permission to koha and to web server
    user (standard: www-data on debian)
    
    Signed-off-by: Zeno Tajoli <tajoli@cilea.it>
    
    http://bugs.koha-community.org/show_bug.cgi?id=7736

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6693,7 +6693,7 @@ if ( CheckVersion($DBversion) ) {
     ");
 
     print "Upgrade to $DBversion done (Bug 7804: Added plugin system.)\n";
-    SetVersion($DBversion);
+    SetVersion ($DBversion);
 }
 
 $DBversion = "3.11.00.107";
@@ -9813,6 +9813,56 @@ if ( CheckVersion($DBversion) ) {
 #   print "Upgrade to $DBversion done (Bug 10877 - Add 'Order Record' processing)\n";
 #   SetVersion ($DBversion);
 #}
+
+=head
+$DBversion = "XXX";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    print "Upgrade to $DBversion done (Add tables for EDI EDIfact ordering)\n";
+my $sql1 = <<"END_EDI1";
+CREATE TABLE IF NOT EXISTS vendor_edi_accounts (
+  id int(11) NOT NULL auto_increment,
+  description text NOT NULL,
+  host text,
+  username text,
+  password text,
+  last_activity date default NULL,
+  provider int(11) default NULL,
+  in_dir text,
+  san varchar(10) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+END_EDI1
+
+my $sql2 = <<"END_EDI2";
+CREATE TABLE IF NOT EXISTS edifact_messages (
+  id int(11) NOT NULL auto_increment,
+  message_type text NOT NULL,
+  date_sent date default NULL,
+  provider int(11) default NULL,
+  status text,
+  basketno int(11) NOT NULL default '0',
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+END_EDI2
+
+my $sql3 = <<"END_EDI3";
+insert into permissions (module_bit, code, description) values (13, 'edi_manage', 'Manage EDIFACT transmissions');
+END_EDI3
+
+my $sql4 = <<"END_EDI4";
+INSERT INTO systempreferences (variable,value,options,explanation,type) VALUES
+('EDIfactEAN', '56781234', '', 'EAN identifier for the library used in EDIfact messages', 'Textarea');
+END_EDI4
+
+    $dbh->do($sql1);
+    $dbh->do($sql2);
+    $dbh->do($sql3);
+    $dbh->do($sql4);
+
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Bug 7736 - Edifact QUOTE and ORDER functionality)\n";
+}
+=cut
 
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
---
+++ misc/edi_files/README.txt
@@ -0,0 +1,3 @@
+This is a readme file but is secretly just a placeholder for the edi_files directory.
+
+--Elliott Davis
