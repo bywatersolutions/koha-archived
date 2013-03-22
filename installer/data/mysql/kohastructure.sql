f61eb1c6b95ded31191eb1c15f625b5e04034c4c
Brendan <info@bywatersolutions.com>
2013-03-22 02:22:11 -0700

    HAMILTON - Bug 7736 - Edifact QUOTE and ORDER functionality
    This is a patch brining together the original
    work by Mark Gavillet and the subsequent patches
    by Mark, Martin Renvoize and myself
    
    It contains the code as currently runnng in
    production
    
    In addition I've added updatedatebase entries to
    installer and replaced the hardcoded currency with
    a currency extraction. As the db currently does not
    force the standard codes we check if that is 3 uppercase
    ascii characters before using it.
    
    Elliott Davis:
    Added unit test for EDI
    
    Zeno Tajoli:
    Rebased on last master git tree.
    I have change only the file installer/data/mysql/sysprefs.sql
    Added missing end tag for tools-home.tt
    
    Brendan Gallagher:
    Rebased on current master
    Changed installer/data/mysql/sysprefs.sql
    koha-tmpl/intranet-tmpl/prog/en/modules/acqui/basket.tt
    
    Bug 7736 - Edifact QUOTE and ORDER functionality - Followup - cordinate this with template tools-home.tt
    
    After the writting of the patch, the template tools-home.tt has a little
    change.
    
    I have have tested the ench wit instruction and I confirm that works.
    Attention on dir misc/edi_files, you need to give permission to koha and to web server
    user (standard: www-data on debian)
    
    Signed-off-by: Zeno Tajoli <tajoli@cilea.it>
    
    http://bugs.koha-community.org/show_bug.cgi?id=7736
    
    Conflicts:
    	acqui/basket.pl
    	installer/data/mysql/de-DE/mandatory/userpermissions.sql
    	installer/data/mysql/es-ES/mandatory/userpermissions.sql
    	installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
    	installer/data/mysql/it-IT/necessari/userpermissions.sql
    	installer/data/mysql/nb-NO/1-Obligatorisk/userpermissions.sql
    	installer/data/mysql/pl-PL/mandatory/userpermissions.sql
    	installer/data/mysql/ru-RU/mandatory/permissions_and_user_flags.sql
    	installer/data/mysql/uk-UA/mandatory/permissions_and_user_flags.sql
    	koha-tmpl/intranet-tmpl/prog/en/includes/admin-menu.inc
    	koha-tmpl/intranet-tmpl/prog/en/modules/acqui/basket.tt
    	koha-tmpl/intranet-tmpl/prog/en/modules/admin/admin-home.tt
    	t/Calendar.t

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -3366,6 +3366,36 @@ CREATE TABLE uploaded_files (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Table structure for table `vendor_edi_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS vendor_edi_accounts (
+  id int(11) NOT NULL auto_increment,
+  description text NOT NULL,
+  host text,
+  username text,
+  password text,
+  last_activity date default NULL,
+  provider int(11) default NULL,
+  in_dir text,
+  san varchar(10) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `edifact_messages`
+--
+CREATE TABLE IF NOT EXISTS edifact_messages (
+  id int(11) NOT NULL auto_increment,
+  message_type text NOT NULL,
+  date_sent date default NULL,
+  provider int(11) default NULL,
+  status text,
+  basketno int(11) NOT NULL default '0',
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Table structure for table linktracker
 -- This stores clicks to external links
 --
