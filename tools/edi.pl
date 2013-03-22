ff1c67b60034c321cb4fdd59b27fcdb5afedfd23
Brendan <info@bywatersolutions.com>
2013-03-22 02:22:11 -0700

    ROSEVILLE - Bug 7736 - Edifact QUOTE and ORDER functionality
    This is a patch brining together the original
    work by Mark Gavillet and the subsequent patches
    by Mark, Martin Renvoize and myself
    
    It contains the code as currently runnng in
    production
    
    In addition I've added updatedatebase entries to
    installer and replaced the hardcoded currency with
    a currency extraction. As the db currently does not
    force the standard codes we check if that is 3 uppercase
    ascii characters before using it.
    
    Elliott Davis:
    Added unit test for EDI
    
    Zeno Tajoli:
    Rebased on last master git tree.
    I have change only the file installer/data/mysql/sysprefs.sql
    Added missing end tag for tools-home.tt
    
    Brendan Gallagher:
    Rebased on current master
    Changed installer/data/mysql/sysprefs.sql
    koha-tmpl/intranet-tmpl/prog/en/modules/acqui/basket.tt
    
    Bug 7736 - Edifact QUOTE and ORDER functionality - Followup - cordinate this with template tools-home.tt
    
    After the writting of the patch, the template tools-home.tt has a little
    change.
    
    I have have tested the ench wit instruction and I confirm that works.
    Attention on dir misc/edi_files, you need to give permission to koha and to web server
    user (standard: www-data on debian)
    
    Signed-off-by: Zeno Tajoli <tajoli@cilea.it>
    
    http://bugs.koha-community.org/show_bug.cgi?id=7736
    
    Conflicts:
    	acqui/basket.pl
    	installer/data/mysql/de-DE/mandatory/userpermissions.sql
    	installer/data/mysql/es-ES/mandatory/userpermissions.sql
    	installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
    	installer/data/mysql/it-IT/necessari/userpermissions.sql
    	installer/data/mysql/nb-NO/1-Obligatorisk/userpermissions.sql
    	installer/data/mysql/pl-PL/mandatory/userpermissions.sql
    	installer/data/mysql/ru-RU/mandatory/permissions_and_user_flags.sql
    	installer/data/mysql/uk-UA/mandatory/permissions_and_user_flags.sql
    	koha-tmpl/intranet-tmpl/prog/en/includes/admin-menu.inc
    	koha-tmpl/intranet-tmpl/prog/en/modules/acqui/basket.tt
    	koha-tmpl/intranet-tmpl/prog/en/modules/admin/admin-home.tt
    	t/Calendar.t

---
+++ tools/edi.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+# Copyright 2011 Mark Gavillet & PTFS Europe Ltd
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+use CGI;
+use C4::Auth;
+use C4::Output;
+use C4::EDI qw( GetEDIfactMessageList);
+
+my $input = CGI->new();
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => 'tools/edi.tmpl',
+        query           => $input,
+        type            => 'intranet',
+        authnotrequired => 0,
+        flagsrequired   => { borrowers => 1 },
+    }
+);
+
+my $messagelist = GetEDIfactMessageList();
+
+$template->param( messagelist => $messagelist );
+
+output_html_with_http_headers $input, $cookie, $template->output;
