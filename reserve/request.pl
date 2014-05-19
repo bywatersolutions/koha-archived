bc6d068820e205c29ca7efe1e8a45127d32b914d
Kyle M Hall <kyle@bywatersolutions.com>
2014-05-19 07:01:58 -0400

    PASCO - Bug 12287 - Hold priority for new hold incorrect if record has holds in transit
    When placing a new hold, the listed priority for the hold will be
    incorrect if there are any holds with items in transit ( i.e. found = T ).
    
    Test Plan:
    1) Place 3 holds on a record with at least 2 items
    2) Fill one hold so it is marked as waiting
    3) Fill on hold so it is marked as in transit
    4) Begin placing a new hold
    5) Note the expected priority would be 2, but is actually 3
    6) Apply this patch
    7) Reload the patch, now the expected priority shows correctly
    
    Signed-off-by: Coralie Barsacq <c.barsacq@mairie-laciotat.fr>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>

--- reserve/request.pl
+++ reserve/request.pl
@@ -203,7 +203,7 @@ foreach my $biblionumber (@biblionumbers) {
     my $alreadyreserved = 0;
 
     foreach my $res (@$reserves) {
-        if ( defined $res->{found} && $res->{found} eq 'W' ) {
+        if ( defined $res->{found} ) {
             $count--;
         }
 
