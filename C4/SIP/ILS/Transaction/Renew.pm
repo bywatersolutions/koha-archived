b0f314f2d0da9489510955798036d5b2983b10e9
Kyle M Hall <kyle@bywatersolutions.com>
2014-11-20 12:39:21 -0500

    Bug 13315 - Add feedback for last item checked out to circulation.pl
    It seems that many librarians find it disconcerting to have no feedback
    with the new checkouts table. It seems that many of them wait for it to
    fully load, check to verify the item was checked out, and only then
    check out the next item.
    
    To help alleviate this issue, we can have the checkouts page give
    feedback about the item that was just checked out.
    
    Test Plan:
    1) Apply this patch
    2) Check an item out
    3) Note the message "$title ($barcode) due on $date_due"
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    This works well and fixes a very problematic issue with the new AJAX
    circ. I will be submitting a follow-up which I think is an improvement
    to the display.
    
    Signed-off-by: Jason Burds  <jburds@dubuque.lib.ia.us>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

--- C4/SIP/ILS/Transaction/Renew.pm
+++ C4/SIP/ILS/Transaction/Renew.pm
@@ -11,6 +11,7 @@ use ILS;
 
 use C4::Circulation;
 use C4::Members;
+use Koha::DateUtils;
 
 use parent qw(ILS::Transaction);
 
@@ -36,7 +37,8 @@ sub do_renew_for  {
     my ($renewokay,$renewerror) = CanBookBeRenewed($borrower->{borrowernumber},$self->{item}->{itemnumber});
     if ($renewokay){
         $self->{due} = undef;
-        my $due_date = AddIssue( $borrower, $self->{item}->id, undef, 0 );
+        my $issue = AddIssue( $borrower, $self->{item}->id, undef, 0 );
+        my $due_date = dt_from_string( $issue->date_due() );
         if ($due_date) {
             $self->{due} = $due_date;
         }
