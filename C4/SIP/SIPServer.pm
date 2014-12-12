478d7433b761ef3002e6b413d2f54aa80c07c74d
“ByWater <staff@bywatersolutions.com>
2014-12-12 08:27:35 -0600

    Revert "SIP Server does not respect timeout setting"
    This reverts commit ec8e83c49b6a04d6f19fc7e25b859ffc52770f63.

--- C4/SIP/SIPServer.pm
+++ C4/SIP/SIPServer.pm
@@ -240,7 +240,6 @@ sub sip_protocol_loop {
 	my $self = shift;
 	my $service = $self->{service};
 	my $config  = $self->{config};
-    my $timeout = $self->{service}->{timeout} || $config->{timeout} || 30;
 	my $input;
 
     # The spec says the first message will be:
@@ -257,10 +256,8 @@ sub sip_protocol_loop {
 	# 
 	# In short, we'll take any valid message here.
 	#my $expect = SC_STATUS;
-    local $SIG{ALRM} = sub { die "SIP Timed Out!\n"; };
     my $expect = '';
     while (1) {
-        alarm $timeout;
         $input = Sip::read_SIP_packet(*STDIN);
         unless ($input) {
             return;		# EOF
@@ -286,7 +283,6 @@ sub sip_protocol_loop {
 		}
 		# We successfully received and processed what we were expecting
 		$expect = '';
-    alarm 0;
 	}
 }
 
