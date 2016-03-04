24f3059a9a02676ebba9cd199e982194ca7cada3
Kyle M Hall <kyle@bywatersolutions.com>
2016-03-04 15:51:49 +0000

    PASCO - Remove atomic updates so they do not get re-run

--- installer/data/mysql/atomicupdate/bug_14373.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO systempreferences(variable,value,options,explanation,type) VALUES
-('DisallowPaymentsOnAccruingFines',  '1',  '',  'If enabled, accruing fines will be unpayable until they are no longer accruing',  'YesNo');
