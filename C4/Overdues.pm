15a382bf487abd10e4dbd73134529c3e086ca5bf
Kyle M Hall <kyle@bywatersolutions.com>
2016-05-10 19:05:00 +0000

    ROUND ROCK - Bug 16496 - accountlines.issue_id not set when new overdue is processed
    It appears that somehow the adding of issue_id to accountlines for new
    fines was missed! This is incredibly important, otherwise UpdateFine
    will always create a new fine!
    
    Test Plan:
    1) Create a new overdue checkout
    2) Run fines.pl to generate an accountline for it
    3) Note it has no issue_id
    4) Apply this patch
    5) Repeat steps 1 and 2
    6) Note it now has an issue_id!
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- C4/Overdues.pm
+++ C4/Overdues.pm
@@ -617,6 +617,7 @@ sub UpdateFine {
                     amountoutstanding => $amount,
                     lastincrement     => $amount,
                     accountno         => $nextaccntno,
+                    issue_id          => $issue_id,
                 }
             )->store();
         }
